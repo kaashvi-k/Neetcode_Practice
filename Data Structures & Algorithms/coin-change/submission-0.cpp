f2e5d1dc8b440f1a2a340ca1952787e5af710867
kaashvi-k <155711416+kaashvi-k@users.noreply.github.com>
2026-07-18 21:40:46 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    unordered_map<int, int> memo; 
+
+    int sol(vector<int> coins, int amount){
+
+        if(amount == 0) return 0; 
+
+        if (memo.find(amount) != memo.end()) return memo[amount];
+
+        int res = INT_MAX; 
+        for(int coin : coins){
+            if(amount - coin >=0 ) {
+                int result = sol(coins, amount - coin);
+
+                if(result != INT_MAX) {
+                    res = min(res , 1 + result);
+                }
+            }
+        }
+
+
+        memo[amount] = res; 
+        return res; 
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+
+        int minCoins = sol(coins, amount);
+        return (minCoins >= INT_MAX) ? -1: minCoins; 
+
+        
+    }
+};
